d201414a2f21b60cc8e6e347db7869b10bd6754f
Benit0Trdlla <benilorenzo007op@gmail.com>
2024-03-17 16:45:27 -0300

    Iniciar cronómetro con boton. Mostrar y ocultar las actividades.

--- script.js
+++ script.js
@@ -1,32 +1,12 @@
 // const alert = document.getElementById("alert");
 // const alert2 = document.getElementById("alert2");
-const buttons = document.querySelectorAll(".btn");
-
-// const respuestasCorrectas = new Set(["7"]); // Definir aquí las respuestas correctas
-
-// function showAlert(numero) {
-//     alert.classList.toggle("d-none");
-//     alert.classList.remove("alert-danger", "alert-success");
-//     alert.classList.add(respuestasCorrectas.has(numero) ? "alert-success" : "alert-danger");
-//     alert.querySelector("strong").textContent = respuestasCorrectas.has(numero) ? "Respuesta correcta!" : `La respuesta correcta era: ${respuestasCorrectas.values().next().value}`;
-//     alert.querySelector("a").classList.toggle("d-none", respuestasCorrectas.has(numero));
-// }
-
-// buttons.forEach((button) => {
-//     button.addEventListener("click", () => {
-//         const numero = button.textContent.trim();
-//         showAlert(numero);
-//     });
-// });
-
-// const alert = document.getElementById("alert");
+// const buttons = document.querySelectorAll(".btn");
 
 
 function setupButtonGroup(buttons, alert, respuestasCorrectas) {
     function showAlert(respuesta) {
         alert.classList.toggle("d-none");
         alert.classList.remove("alert-danger", "alert-success");
-        // console.log(respuestasCorrectas.has(respuesta))
         if (respuestasCorrectas.has(respuesta)) {
             alert.classList.add("alert-success");
         } else {
@@ -53,3 +33,24 @@ const group2Buttons = document.querySelectorAll(".group2 .btn");
 const respuestasCorrectasGroup2 = new Set(["(X-3)(x+3)"]); // Definir aquí las respuestas correctas para el grupo 2
 const alertGroup2 = document.getElementById("alert2");
 setupButtonGroup(group2Buttons, alertGroup2, respuestasCorrectasGroup2);
+
+let number = 30;
+let intervalId;
+
+const timerElement = document.querySelector(".bg-warning p"); 
+const buttonTimer = document.getElementById("cronometrar");
+const accordionFlush = document.getElementById("accordionFlush");
+
+buttonTimer.addEventListener("click", () => {
+    accordionFlush.classList.toggle("d-none");
+    clearInterval(intervalId); // Limpiar el intervalo existente para evitar múltiples intervalos activos
+    intervalId = setInterval(() => {
+        timerElement.textContent = `Tu tiempo es de ${number} segundos`; // Mostrar el número actual en el elemento del cronómetro
+        if (number === 0) {
+            clearInterval(intervalId);
+            timerElement.textContent = `Se acabo el tiempo`; // Mostrar el número actual en el elemento del cronómetro
+            accordionFlush.classList.toggle("d-none");
+        }
+        number--;
+    }, 1000);
+});
